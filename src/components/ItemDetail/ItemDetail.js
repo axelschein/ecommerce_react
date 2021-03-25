aacf3f55a6220ccbd421de0aef54a308770fc44a
Axel Scheinsohn <axelschein@gmail.com>
2021-03-24 21:55:47 -0300

    arreglo la foto del itemDetail

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import "./ItemDetail.css";
 
 export default function ItemDetail({ item }) {
  
     return <>
             <div className='container-img2'>
-                <img src={item?.pictureUrl} alt="Cupcake"/>
+                <img className="img2" src={item?.pictureUrl} alt="Cupcake"/>
             </div>
             <h2>{item?.name}</h2>
             <p>{item?.description}</p>
