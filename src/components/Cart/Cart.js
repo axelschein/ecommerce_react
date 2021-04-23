2d075d895110b05572da56fc97a27254f5f183b8
Axel Scheinsohn <axelschein@gmail.com>
2021-04-22 23:23:33 -0300

    desafio firebase II, falta terminar tengo un error despues de finalizar la compra

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,11 +2,49 @@ import React, { useContext } from "react";
 import { CartContext } from "../../context/CartContext";
 import { Link } from "react-router-dom";
 import "./Cart.css";
+import firebase from 'firebase/app'
+import 'firebase/firebase'
+import { getFirestore } from "../../firebase";
 
 
 export const Cart = () => {
 
+    // {
+    //     buyer: {name: 'Juan', phone: 'mi tel',}
+    // }
+
     const {cart,removeItem,cartLength,cartPrice,clear} = useContext(CartContext)
+
+    const generarOrden = () => {
+        const db = getFirestore();
+
+        const ordersCol = db.collection('orders')
+        let orden = {}
+        orden.date = firebase.firestore.Timestamp.fromDate(new Date());
+
+        orden.buyer = { name:'Juan', phone:'mi telefono', email:'pepe86@AOL.com.ar' }
+        orden.total = cartPrice;
+        orden.items = cart.map(cartItem => {
+            const id = cartItem.item.id;
+            const title = cartItem.item.title;
+            const price = cartItem.item.price * cart.quantity;
+            return {id, title, price}   
+        })
+
+        ordersCol.add(orden)
+        .then((IdDocumento) => {
+            console.log({IdDocumento})
+        })
+        .catch( err => {
+            console.log(err);
+        })
+        .finally(()=> {
+            console.log('se termino la promesa')
+        })
+
+        console.log(orden);
+
+    }
 
     const noItemComp = <h2>No hay Items en el carrito <Link to='/'>Ir al home </Link> </h2>;
 
@@ -28,7 +66,9 @@ export const Cart = () => {
             )}
                 <div> Cantidad:{cartLength} </div> 
                 <div> Total: ${cartPrice}</div> 
-                <button onClick={clear}>Borrar todo</button>         
+                <button onClick={clear}>Borrar todo</button>    
+
+                <button onClick={generarOrden}>Finalizar Compra</button>     
                 {/* </>
             )
             } */}
